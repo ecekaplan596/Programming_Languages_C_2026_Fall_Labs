c4e6090f3dbf05d294c0f1041198c6d6c264bdb6
ecekaplan596 <ecekaplan596@gmail.com>
2026-09-23 13:05:12 +0000

    lab2

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
-/*
-    Task:
-    Write a function `int sum_to_n(int n)` that computes
-    the sum of all integers from 1 up to n using a for loop.
-
-    In main():
-      - Ask user for a positive integer n
-      - If n < 1, print an error
-      - Otherwise, call sum_to_n and print the result
-*/
-
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+    int sum= 0;
+
+    for(int i= 1; i <=n; i++){
+        sum += i;
+    }
+    return sum;
 }
 
 int main(void) {
@@ -21,8 +14,12 @@ int main(void) {
 
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
-
-    // TODO: validate input, call function, and print result
+    if (n < 1){
+        printf("error: n must be bigger than 0.\n");
+    }
+    else{
+        printf("sum from 1 to %d = %d\n", n, sum_to_n(n));
+    }
 
     return 0;
 }
--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,8 +12,11 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+    long long result = 1 ;
+    for (int i=1; i <=n; i++){
+        result *= i;
+    }
+    return result;
 }
 
 int main(void) {
@@ -22,7 +25,11 @@ int main(void) {
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
 
-    // TODO: validate input, call function, print result
-
+    if(n<0){
+        printf("error: n can't be negative.\n");
+    }
+    else{
+        printf("factorial of %d = %d\n", n, factorial(n));
+    } 
     return 0;
 }
